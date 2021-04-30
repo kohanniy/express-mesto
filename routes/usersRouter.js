d663292443aa3d366fba1735963dbfac06a9200d
Вячеслав Коханный <xoxluwa1@yandex.com>
2021-04-30 18:09:41 +0500

    проверка 26

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,9 +1,9 @@
 const usersRouter = require('express').Router();
-const { getUsers, getMe, getUser, updateProfile, updateAvatar } = require('../controllers/users');
+const { getUsers, getUser, updateProfile, updateAvatar } = require('../controllers/users');
 const { checkId, checkUpdateProfile, checkUpdateAvatar } = require('../middlewares/userRequestValidation');
 
 usersRouter.get('/users', getUsers);
-usersRouter.get('/users/me', getMe);
+// usersRouter.get('/users/me', getMe);
 usersRouter.patch('/users/me', checkUpdateProfile, updateProfile);
 usersRouter.patch('/users/me/avatar', checkUpdateAvatar, updateAvatar);
 usersRouter.get('/users/:id', checkId, getUser);
